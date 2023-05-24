24b4520ceeed0afd5ecfb4e9fba4155272819836
KarolEs1824 <133879096+KarolEs1824@users.noreply.github.com>
2023-05-24 08:15:32 -0500

    Update karol.py

--- karol.py
+++ karol.py
@@ -1,1 +1,13 @@
+def calcular_division(dividendo, divisor):
+    try:
+        resultado = dividendo / divisor
+        return resultado
+    except ZeroDivisionError:
+        print("Error: No es posible dividir entre cero.")
+
+# Ejemplo de uso
+dividendo = 10
+divisor = 2
+resultado = calcular_division(dividendo, divisor)
+print("El resultado de la división es:", resultado)
 
